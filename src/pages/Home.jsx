f6ba9039284074a1cff43c49b1018fe752ca3ae4
Mohammed Mudabbir Pasha <116947092+mudabbir525@users.noreply.github.com>
2024-09-06 21:27:00 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Home.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Menu from "./Menu"; 
+
 import burger1 from "../assets/burger1.png";
 import pizza from "../assets/pizza.png";
 import shawarma from "../assets/shawarma.png";
@@ -10,6 +10,8 @@ import mandi from "../assets/mandi-rice.png";
 import drinks from '../assets/drinks.png';
 
 const Home = () => {
+   const navigate = useNavigate();
+  
   useEffect(() => {
     const boxes = document.querySelectorAll('.boxx');
     
