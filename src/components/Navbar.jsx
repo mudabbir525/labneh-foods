82891c6ee3805b369fe088c2723487ab80512708
Mohammed Mudabbir Pasha <116947092+mudabbir525@users.noreply.github.com>
2024-10-02 01:42:37 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './nav.css'; // Custom CSS for futuristic styling
 import logo from '../assets/labneh.png';
+
 const Navbar = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
 
@@ -11,21 +12,26 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
+      {/* Fixing the class name issue here */}
       <button className={`toggle-button ${isSidebarOpen ? 'active' : ''}`} onClick={toggleSidebar}>
         ☰
       </button>
       <div className="navbar-brand">
-        <span className="brand-glow"><img src={logo} alt="" /></span>
+        <span className="brand-glow"><img src={logo} alt="Labneh Logo" /></span>
       </div>
+      
       {/* Franchise Call Information */}
       <div className="franchise-info">
         Call for franchise On: +91 98107 01117
       </div>
+
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Menu</Link></li>
         <li><Link to="/contact-us">Contact Us</Link></li>
       </ul>
+
+      {/* Fixing the sidebar class name */}
       <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
         <button className="close-button" onClick={toggleSidebar}>
           &times;
