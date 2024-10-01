8b3df7a6a531bc33302beaf467d8b1b9bab72838
Mohammed Mudabbir Pasha <116947092+mudabbir525@users.noreply.github.com>
2024-10-01 20:48:19 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import kunaffa from "../assets/KunaffaGridmin.jpg";
 import shawarma from "../assets/shawarmaGridmin.jpeg";
 import shawarma1 from "../assets/shawarma.png";
 import salad from "../assets/saladGridmin.JPG";
-import mandi from "../assets/mandiGridmin.JPG";
+import mandi from "../assets/mandiGrid-min.JPG";
 import platter from "../assets/MezzePlattersGrid-min.jpg";
 import pizza from "../assets/pizza.png";
 import burger1 from "../assets/burger1.png";
